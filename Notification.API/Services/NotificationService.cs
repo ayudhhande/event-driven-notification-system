3a463daf01fe91801011232cb0b77bbf313ee1ac
Ayudh Hande <7057557+ayudhhande@users.noreply.github.com>
2026-04-08 22:40:36 +0530

    Add notification entity logic in CreateNotificationAsync method.

--- Notification.API/Services/NotificationService.cs
+++ Notification.API/Services/NotificationService.cs
@@ -17,8 +17,28 @@ public class NotificationService : INotificationService
     public async Task CreateNotificationAsync(NotificationRequest request)
     {
         _logger.LogInformation($"Creating notification for User {request.UserId} with event type {request.EventType}");
-        var notification = new NotificationEntity();
-        await _repo.AddAsync(notification);
+        var entity = new NotificationEntity
+        {
+            Id = Guid.NewGuid(),
+            UserId = request.UserId,
+            EventType = request.EventType,
+            Message = request.Message,
+            Status = PaymentStatus.PENDING,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var paymentEntity = new PaymentEvent
+        {
+            EventId = Guid.NewGuid(),
+            UserId = request.UserId,
+            EventType = request.EventType,
+            Message = request.Message,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        await _repo.AddAsync(entity);
+
+        //TODO: Publish event to kafka
     }
     
 }
