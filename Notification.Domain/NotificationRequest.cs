825ff414a24e276423961f52decebec0c38dd38f
Ayudh Hande <7057557+ayudhhande@users.noreply.github.com>
2026-04-08 21:52:38 +0530

    Add validations

--- Notification.Domain/NotificationRequest.cs
+++ Notification.Domain/NotificationRequest.cs
@@ -1,11 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Notification.Domain;
 
 public class NotificationRequest
 {
+    [Required]
     public int UserId { get; set; }
+    [Required]
     public PaymentEventType EventType { get; set; }
+    [Required]
+    [MaxLength(500)]
     public string Message { get; set; }
     public PaymentStatus Status { get; set; }
 }
