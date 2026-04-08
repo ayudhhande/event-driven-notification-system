3a039df9886123ba7ad53d4924c2b4d92a6223a0
Ayudh Hande <7057557+ayudhhande@users.noreply.github.com>
2026-04-08 22:15:39 +0530

    Add logs in notification service.

--- Notification.API/Services/NotificationService.cs
+++ Notification.API/Services/NotificationService.cs
@@ -7,13 +7,16 @@ namespace Notification.API.Services;
 public class NotificationService : INotificationService
 {
     private readonly INotificationRepository _repo;
-    public NotificationService(INotificationRepository repo)
+    private readonly ILogger _logger;
+    public NotificationService(INotificationRepository repo, ILogger logger)
     {
         _repo = repo;
+        _logger = logger;
     }
 
     public async Task CreateNotificationAsync(NotificationRequest request)
     {
+        _logger.LogInformation($"Creating notification for User {request.UserId} with event type {request.EventType}");
         var notification = new NotificationEntity();
         await _repo.AddAsync(notification);
     }
