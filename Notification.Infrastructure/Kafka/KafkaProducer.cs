3c8d7323e5b589f9ca51ea56a98f39c49ba319ae
Ayudh Hande <7057557+ayudhhande@users.noreply.github.com>
2026-05-05 22:07:39 +0530

    Makes the topic name configurable and add logs to kafka producer

--- Notification.Infrastructure/Kafka/KafkaProducer.cs
+++ Notification.Infrastructure/Kafka/KafkaProducer.cs
@@ -1,15 +1,20 @@
 using System;
 using Confluent.Kafka;
 using System.Text.Json;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Notification.Infrastructure.Kafka;
 
 public class KafkaProducer: IKafkaProducer
 {
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<string> _logger;
     public IProducer<Null, string> _producer;
-    public const string Topic = "payment-events";
+    // public const string Topic = "payment-events";
 
-    public KafkaProducer()
+    public KafkaProducer(IConfiguration configuration, ILogger<string> logger)
     {
         var config = new ProducerConfig
         {
@@ -17,16 +22,28 @@ public class KafkaProducer: IKafkaProducer
         };
 
         _producer = new ProducerBuilder<Null, string>(config).Build();
+        _configuration = configuration;
+        _logger = logger;
     }
 
     public async Task PublishAsync<T>(T message)
     {
-        var json = JsonSerializer.Serialize(message);
-
-        await _producer.ProduceAsync(Topic, new Message<Null, string>
+        try
         {
-            Value = json
-        });
-        Console.WriteLine($"Publishing message to Kafka: {json}");
+            var json = JsonSerializer.Serialize(message);
+            string Topic = _configuration["Kafka:Topic"] ?? string.Empty;
+            await _producer.ProduceAsync(Topic, new Message<Null, string>
+            {
+                Value = json
+            });
+            Console.WriteLine($"Publishing message to Kafka: {json}");
+            _logger.LogInformation("Publishing message to Kafka {0}", message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to publish event");
+            throw;
+        }
+        
     }
 }
